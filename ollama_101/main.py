69ed95a09591a545ba20202a9d5d68fb500abeba
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2024-11-04 16:03:59 +0100

    Added more verbose print, and more inputs

--- ollama_101/main.py
+++ ollama_101/main.py
@@ -1,14 +1,24 @@
 import ollama
 from ollama import generate
-print("llama3.2 chat:")
+import random
+
+
+question = random.choice([
+    "Why is the sky blue?",
+    "How many of the letter r, are there in the word 'Strawberry'?",
+    "Can you tell me a joke?",
+    "Can you write java code to print the 10 first fibonacci numbers?",
+])
+
+print(f"Asking llama3.2: {question}")
 response = ollama.chat(model='llama3.2', messages=[
   {
     'role': 'user',
-    'content': 'Why is the sky blue?',
+    'content': question,
   },
 ])
 print(response['message']['content'])
 
-print("phi3.5 chat:")
-response = generate('phi3.5', 'why is the sky blue?')
+print(f"Asking phi3.5: {question}")
+response = generate('phi3.5', question)
 print(response['response'])
